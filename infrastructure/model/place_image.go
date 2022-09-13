6a2e0fe380ac1db0f6e86b2280fda02d8b1c25a7
1saifj <saifalialjanahi@gmail.com>
2022-09-13 19:00:26 +0300

    Add place image

---
+++ infrastructure/model/place_image.go
@@ -0,0 +1,8 @@
+package model
+
+type Image struct {
+	Model
+	PlaceID uint64 `json:"place_id" gorm:"not null"`
+	Image   string `json:"image" gorm:"type:varchar(255);not null"`
+	Place   Place  `gorm:"foreignKey:PlaceID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"place"`
+}
