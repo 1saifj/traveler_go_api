197d051ab6f229f28dc750b847e848a8244c98e1
1saifj <saifalialjanahi@gmail.com>
2022-09-13 18:59:53 +0300

    Edit app and database configuration

--- infrastructure/datastore/db.go
+++ infrastructure/datastore/db.go
@@ -1,47 +1,27 @@
 package datastore
 
 import (
-	"errors"
-	"fmt"
 	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"strings"
 	"taveler/config"
 	"taveler/infrastructure/model"
 )
 
-func SetupDB(con *config.DatabaseConfig) (*gorm.DB, error) {
+func SetupDB() (*gorm.DB, error) {
 	var dialector gorm.Dialector
-	switch strings.ToLower(con.Driver) {
-	case "postgresql":
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%v TimeZone=%s",
-			con.Host, con.Username, con.Password, con.Name, con.Port, con.SslMode, con.Timezone)
-		dialector = postgres.Open(dsn)
-		break
-	case "sqlite":
-		if !strings.HasSuffix(con.Name, ".db") {
-			con.Name += ".db"
-		}
-		dialector = sqlite.Open(con.Name)
-		break
-	default:
-		return nil, errors.New("invalid DB driver")
-	}
-
+	dbConfig := config.DatabaseConfig{}
+	dialector = postgres.Open(dbConfig.GetDSN())
 	db, err := gorm.Open(dialector, &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-
 	err = migrate(db)
 	if err != nil {
 		return nil, err
 	}
-
 	return db, nil
 }
 
 func migrate(db *gorm.DB) error {
-	return db.AutoMigrate(model.Place{}, model.Category{})
+	return db.AutoMigrate(model.Place{}, model.Category{}, model.Image{})
 }
